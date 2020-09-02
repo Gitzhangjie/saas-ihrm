35c7ccec5d92b8319d528ac1bc69a01ccabb5884
zhangjie <1357270154@qq.com>
2020-09-02 20:39:16 +0800

    xx

---
+++ ihrm_company/src/main/java/com/ihrm/company/controller/CompanyController.java
@@ -0,0 +1,69 @@
+package com.ihrm.company.controller;
+
+import com.ihrm.common.entity.Result;
+import com.ihrm.common.entity.ResultCode;
+import com.ihrm.company.service.CompanyService;
+import com.ihrm.domain.company.Company;
+import com.sun.org.apache.xml.internal.security.keys.keyresolver.implementations.PrivateKeyResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/company")
+public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    //保存企业
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Result save(@RequestBody Company company) {
+        //业务操作
+        companyService.add(company);
+        return new Result(ResultCode.SUCCESS);
+    }
+    //根据ID更新企业
+    /**
+     * 1.方法
+     * 2.请求参数
+     * 3.响应
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Result update(@PathVariable(value = "id") String id,@RequestBody Company company){
+        company.setId(id);
+        companyService.update(company);
+        return new Result(ResultCode.SUCCESS);
+    }
+
+    //根据id删除企业
+    /**
+     *
+     */
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public Result delete(@PathVariable(value = "id") String id){
+
+        companyService.deleteById(id);
+        return new Result(ResultCode.SUCCESS);
+    }
+
+    //根据ID查询企业
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Result findById(@PathVariable(value = "id") String id){
+        Company company = companyService.findById(id);
+        Result result = new Result(ResultCode.SUCCESS);
+        result.setData(company);
+        return result;
+    }
+    //查询企业列表
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public Result findAll(){
+        List<Company> list = companyService.findAll();
+        Result result = new Result(ResultCode.SUCCESS);
+        result.setData(list);
+        return result;
+    }
+
+}
